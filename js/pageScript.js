fde6abd78ea4c7f33f81f83c80bb5589c0bb9682
Yusuf Abdulazeez <yusuf.abdulazeez1912@gmail.com>
2020-04-26 06:08:43 +0100

    Initial commit

---
+++ js/pageScript.js
@@ -0,0 +1,51 @@
+/*function testPage() {
+	window.onload = prompt('Type your name');
+}
+
+testPage();*/
+//Login/Register Form DOM
+const loginBorder = document.getElementById('login');
+const regBorder = document.getElementById('register');
+const login = document.querySelector('.login');
+const register = document.querySelector('.register')
+
+//Login/Register Form Events
+loginBorder.addEventListener('click', () => {
+	loginBorder.classList.remove('button-border');
+	regBorder.classList.add('button-border');
+	login.classList.remove('display');
+	register.classList.add('display');
+});
+
+regBorder.addEventListener('click', () => {
+	loginBorder.classList.add('button-border');
+	regBorder.classList.remove('button-border');
+	login.classList.add('display');
+	register.classList.remove('display');
+})
+
+register.classList.add('display');
+regBorder.classList.add('button-border');
+/*body.style.backgroundColor = 'transparent';*/
+
+//Hamburder Menu DOM
+const menuBtn = document.querySelector('.hamburger');
+const menus = document.querySelector('.nav-2-item');
+const navBar = document.querySelector('.nav-2');
+
+//Hamburger Menu Events
+menuBtn.addEventListener('click', () => {
+	if (navBar.style.display === 'none') {
+		navBar.style.display = 'block';
+	 
+		menus.forEach(menu => {
+			menu.style.display = 'block';
+			//menu.classList.toggle('fade');
+		});	
+	}
+	else {
+		navBar.style.display = 'none';	
+	}
+	
+});
+
